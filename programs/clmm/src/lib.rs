453420768bec3addd939612ac7f60244b3d31b13
its-YogeshChandra <pandittheroyal@gmail.com>
2026-01-22 02:35:37 +0530

    edited file structure of distributed logc

--- programs/clmm/src/lib.rs
+++ programs/clmm/src/lib.rs
@@ -1,9 +1,11 @@
 use anchor_lang::prelude::*;
 use anchor_spl::{
     associated_token::AssociatedToken,
-    token::{self, Token, TokenAccount},
+    token::{self},
     token_interface::{self, Burn, Mint, MintTo, TokenAccount, TokenInterface, TransferChecked},
 };
+
+mod errors;
 
 declare_id!("CicZMzrBxTazWhSXGKXkkbnRiYFXSXm2Pe47RvW1X3qt");
 
@@ -21,7 +23,7 @@ pub mod clmm {
     }
 }
 
-//include ticks 
+//include ticks
 #[account]
 #[derive(InitSpace)]
 pub struct LpPoolStateShape {
@@ -36,8 +38,8 @@ pub struct LpPoolStateShape {
     //bump
     bump: u8,
 
-    //configuration 
-    tick_spacing : u16,
+    //configuration
+    tick_spacing: u16,
     fee_rate: u32,
 
     //current state
@@ -45,41 +47,41 @@ pub struct LpPoolStateShape {
     tick_current: i32,
     liquidity: u128,
 
-   //fee tracking
-    fee_growth_global_0:u128,
-    fee_growth_global_1: u128
+    //fee tracking
+    fee_growth_global_0: u128,
+    fee_growth_global_1: u128,
 }
 
-//for managing the ticks 
+//for managing the ticks
 #[account]
 pub struct TickState {
     initialized: bool,
-    liquidity_gross: u128,  //total liquidity referencing this 
-    liquidity_net : i128, 
+    liquidity_gross: u128, //total liquidity referencing this
+    liquidity_net: i128,
     fee_growth_outside_0: u128,
-    fee_growth_outside_1: u128
+    fee_growth_outside_1: u128,
 }
 
-//tick state array 
+//tick state array
 #[account]
 pub struct TickArrayState {
     pool_id: Pubkey,
-    start_tick_index:i32,
-    ticks:[TickState; 60]
+    start_tick_index: i32,
+    ticks: [TickState; 60],
 }
 
-//design postions 
+//design postions
 #[account]
 pub struct Position {
-    pool_id : Pubkey,
+    pool_id: Pubkey,
     owner: Pubkey,
     tick_lower: i32,
     tick_upper: i32,
     liquidity: u128,
     fee_growth_inside_0_last: u128,
-    fee_growth_inside_1_last : u128,
+    fee_growth_inside_1_last: u128,
     tokens_owed_0: u64,
-    tokens_owed_1: u64
+    tokens_owed_1: u64,
 }
 
 #[derive(Accounts)]
@@ -90,30 +92,26 @@ pub struct Initialize<'info> {
     pub signer: Signer<'info>,
 
     pub usdc_mint: InterfaceAccount<'info, Mint>,
-    pub sol_mint: InterfaceAccount<'info, Mint>,
+    pub wsol_mint: InterfaceAccount<'info, Mint>,
 
     //token program
-    pub token_program: Interface<'info, Token>,
+    pub token_program: Interface<'info, TokenInterface>,
 
     //system program
     pub system_program: Program<'info, System>,
 
-    //lp token_mint
-    #[account(mut)]
-    pub lp_token_mint: InterfaceAccount<'info, Mint>,
-
     // make the pool state
-    #[accounts(init, payer = signer, space= 8+LpPoolStateShape::INIT_SPACE, seeds = [b"pool_state_v1", usdc_mint.key().as_ref(), wsol_mint.key().as_ref()], bump)]
+    #[account(init, payer = signer, space= 8+LpPoolStateShape::INIT_SPACE, seeds = [b"pool_state_v1", usdc_mint.key().as_ref(), wsol_mint.key().as_ref()], bump)]
     pub pool_state_account: Account<'info, LpPoolStateShape>,
 
     //make the vaults
-    #[accounts(init, payer=signer, token::mint = usdc_mint, token::authority = pool_state_account, token::token_program, seeds = [b"usdc_vault", usdc_mint.key().as_ref()], bump)]
+    #[account(init, payer=signer, token::mint = usdc_mint, token::authority = pool_state_account, token::token_program = token_program, seeds = [b"usdc_vault", usdc_mint.key().as_ref()], bump)]
     pub usdc_vault_account: InterfaceAccount<'info, TokenAccount>,
 
-    #[accounts(init, payer=signer, token::mint = usdc_mint, token::authority = pool_state_account, token::token_program, seeds = [b"wsol_vault", wsol_mint.key().as_ref()], bump)]
+    #[account(init, payer=signer, token::mint = usdc_mint, token::authority = pool_state_account, token::token_program = token_program, seeds = [b"wsol_vault", wsol_mint.key().as_ref()], bump)]
     pub wsol_vault_account: InterfaceAccount<'info, TokenAccount>,
 
-    //mint for the lp tokens
+    // creating mint for the lp tokens
     #[account(init, payer = signer, mint::decimals= 9, mint::authority = pool_state_account, mint::freeze_authority = pool_state_account)]
     pub lp_token_mint: InterfaceAccount<'info, Mint>,
 }
@@ -137,7 +135,7 @@ pub struct ProvideLp<'info> {
     pub token_program: Interface<'info, TokenInterface>,
 
     //system program
-    pub system_program: Progam<'info, System>,
+    pub system_program: Program<'info, System>,
 
     //pool_state_account
     pub pool_state_account: Account<'info, LpPoolStateShape>,
