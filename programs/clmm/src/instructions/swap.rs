2e9500e160330d06755e25c20f8a7124cfc2c7fc
its-YogeshChandra <pandittheroyal@gmail.com>
2026-01-22 16:59:59 +0530

    added swap function

---
+++ programs/clmm/src/instructions/swap.rs
@@ -0,0 +1,33 @@
+use anchor_lang::prelude::*;
+use anchor_spl::{
+    associated_token::AssociatedToken,
+    token::{self},
+    token_interface::{self, Burn, Mint, MintTo, TokenAccount, TokenInterface, TransferChecked},
+};
+
+use crate::TickArrayState;
+
+#[derive(Accounts)]
+pub struct Swap<'info> {
+    //account
+    #[account(mut)]
+    pub signer: Signer<'info>,
+
+    //pool state
+    pub pool_state_account: InterfaceAccount<'info, TokenAccount>,
+
+    //user accounts
+    #[account(mut, token::authority = signer)]
+    pub user_input_account: InterfaceAccount<'info, TokenAccount>,
+    #[account(mut, token::authority = signer)]
+    pub user_output_account: InterfaceAccount<'info, TokenAccount>,
+
+    //vault account
+    #[account(mut, token::authority = pool_state_account)]
+    pub input_vault_account: InterfaceAccount<'info, TokenAccount>,
+    #[account(mut , token::authority = pool_state_account)]
+    pub output_vault_account: InterfaceAccount<'info, TokenAccount>,
+
+    //token account
+    pub tickarray: Account<'info, TickArrayState>,
+}
