12742155c59cb8dfce7db58185614d2f25af319f
its-YogeshChandra <pandittheroyal@gmail.com>
2026-01-22 23:38:17 +0530

    fixed the open position struct

--- programs/clmm/src/instructions/open_position.rs
+++ programs/clmm/src/instructions/open_position.rs
@@ -5,11 +5,12 @@ use anchor_spl::{
     token_interface::{self, Burn, Mint, MintTo, TokenAccount, TokenInterface, TransferChecked},
 };
 
+use crate::states::pool::LpPoolStateShape;
 use crate::states::Position;
-use crate::states::{pool::LpPoolStateShape, TickArrayState};
 
 //files
 #[derive(Accounts)]
+#[instruction(tick_lower: i32, tick_upper: i32)]
 pub struct Open_Position<'info> {
     //signer for the value
     #[account(mut)]
@@ -21,10 +22,23 @@ pub struct Open_Position<'info> {
     //pool static
     pub pool_state_account: Account<'info, LpPoolStateShape>,
 
-    //ticks bounds
-    pub tick_upper: Account<'info, TickArrayState>,
-
     //postion
-    #[account(init, payer= signer, space= 8+Position::INIT_SPACE, seeds = [b"position", signer.key().as_ref(), pool_state_account.key().as_ref(),], bump)]
+    #[account(init, payer= signer, space= 8+Position::INIT_SPACE, seeds = [b"position", signer.key().as_ref(), pool_state_account.key().as_ref(),&tick_upper.to_le_bytes(), &tick_lower.to_le_bytes()], bump)]
     pub position: Account<'info, Position>,
+}
+
+impl<'info> Open_Position<'info> {
+    pub fn handler(&mut self, tick_lower: i32, tick_upper: i32) -> Result<()> {
+        let position = &mut self.position;
+        position.pool_id = self.pool_state_account.key();
+        position.owner = self.signer.key();
+        position.tick_lower = tick_lower;
+        position.liquidity = 0;
+        position.tick_upper = tick_upper;
+        position.tokens_owed_0 = 0;
+        position.tokens_owed_1 = 0;
+        position.fee_growth_inside_0_last = 0;
+        position.fee_growth_inside_1_last = 0;
+        Ok(())
+    }
 }
--- programs/clmm/src/states/position.rs
+++ programs/clmm/src/states/position.rs
@@ -4,13 +4,13 @@ use anchor_lang::prelude::*;
 #[account]
 #[derive(InitSpace)]
 pub struct Position {
-    pool_id: Pubkey,
-    owner: Pubkey,
-    tick_lower: i32,
-    tick_upper: i32,
-    liquidity: u128,
-    fee_growth_inside_0_last: u128,
-    fee_growth_inside_1_last: u128,
-    tokens_owed_0: u64,
-    tokens_owed_1: u64,
+    pub pool_id: Pubkey,
+    pub owner: Pubkey,
+    pub tick_lower: i32,
+    pub tick_upper: i32,
+    pub liquidity: u128,
+    pub fee_growth_inside_0_last: u128,
+    pub fee_growth_inside_1_last: u128,
+    pub tokens_owed_0: u64,
+    pub tokens_owed_1: u64,
 }
